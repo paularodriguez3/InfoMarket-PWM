04ca691c67db0f11d19e57218961cfb3bc4a4c67
paularodriguez3 <paularosarodriguezmorales03@gmail.com>
2025-02-10 22:40:30 +0000

    Finished header menu

---
+++ scripts/home/index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const menuIcon = document.querySelector('.fi-rr-menu-burger');
+    const mobileMenu = document.querySelector('#menu-wrapper');
+    const main = document.querySelector('main');
+    const footer = document.querySelector('footer');
+
+    menuIcon.addEventListener('click', function () {
+        mobileMenu.classList.toggle('show-menu');
+        main.classList.toggle('blur-item');
+        footer.classList.toggle('blur-item');
+    });
+});
