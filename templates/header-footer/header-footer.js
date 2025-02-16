84822a76457462d21f99c4bf1fe52f1b8962685d
paularodriguez3 <paularosarodriguezmorales03@gmail.com>
2025-02-16 15:22:52 +0000

    Add header and footer template

--- templates/header-footer/header-footer.js
+++ templates/header-footer/header-footer.js
@@ -2,9 +2,11 @@ document.addEventListener('DOMContentLoaded', () => {
     const menuIcon = document.querySelector('.fi-rr-menu-burger');
     const mobileMenu = document.querySelector('#menu-wrapper');
     const main = document.querySelector('main');
+    const footer = document.querySelector('footer');
 
     menuIcon.addEventListener('click', function () {
         mobileMenu.classList.toggle('show-menu');
         main.classList.toggle('blur-item');
+        footer.classList.toggle('blur-item');
     });
 });
