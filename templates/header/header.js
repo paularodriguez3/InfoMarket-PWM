1ef8feca5eb967b4827d8d977801b33223f9b048
paularodriguez3 <paularosarodriguezmorales03@gmail.com>
2025-02-18 15:49:22 +0000

    Search bar first try

--- templates/header/header.js
+++ templates/header/header.js
@@ -3,7 +3,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const mobileMenu = document.querySelector('#menu-wrapper');
     const main = document.querySelector('main');
     const footer = document.querySelector('footer');
+    const searchIcon = document.querySelector('#search');
+    const searchContainer = document.querySelector('#search-container');
+    const closeSearch = document.querySelector('#close-search');
 
+    // Mostrar la barra de búsqueda
+    searchIcon.addEventListener('click', (event) => {
+        event.preventDefault();
+        searchContainer.classList.remove('hidden');
+    });
+
+    // Cerrar la barra de búsqueda
+    closeSearch.addEventListener('click', () => {
+        searchContainer.classList.add('hidden');
+    });
+
+    // Mantener funcionalidad del menú
     menuIcon.addEventListener('click', function () {
         mobileMenu.classList.toggle('show-menu');
         main.classList.toggle('blur-item');
