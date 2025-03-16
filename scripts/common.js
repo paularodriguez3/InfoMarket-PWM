2e2b62adb62dcfd9ea4695cf8856547a10e724af
Raúl Núñez <raul.nunez101@alu.ulpgc.es>
2025-03-16 17:41:17 +0000

    Fixed imports in all pages

--- scripts/common.js
+++ scripts/common.js
@@ -1,6 +1,6 @@
 async function loadTemplate(template, targetElementId, scriptPath = null) {
     try {
-        const response = await fetch(`../templates/${template}`);
+        const response = await fetch(`../../templates/${template}`);
         const html = await response.text();
         document.getElementById(targetElementId).innerHTML = html;
 
@@ -16,8 +16,13 @@ async function loadTemplate(template, targetElementId, scriptPath = null) {
 }
 
 async function loadCommonTemplates() {
-    await loadTemplate('header/header.html', 'header', '../templates/header/header.js');
+    await loadTemplate('header-2/header-2.html', 'header-2', '../../templates/header-2/header.js');
+    await loadTemplate('header/header.html', 'header', '../../templates/header/header.js');
     await loadTemplate('footer/footer.html', 'footer');
+    await loadTemplate('shopping-info-component/shopping-info-component.html', 'shopping');
+    await loadTemplate('login-component/componente-inicio-sesion.html','sign-in');
+    await loadTemplate('login-component/componente-crear-cuenta.html','sign-up');
+
 }
 
 loadCommonTemplates();
--- templates/header/header.js
+++ templates/header/header.js
@@ -12,28 +12,6 @@ waitForElement("#menu-wrapper", () => {
     const mobileMenu = document.querySelector('#menu-wrapper');
     const main = document.querySelector('main');
     const footer = document.querySelector('footer');
-    const search = document.querySelector('.search');
-    const btn = document.querySelector('.btn');
-    const input = document.querySelector('.input');
-
-    btn.addEventListener('click', () => {
-        if (search.classList.contains('active')) {
-            search.classList.remove('active');
-            input.blur();
-        } else {
-            search.classList.add('active');
-            setTimeout(() => {
-                input.focus();
-            }, 50);
-        }
-    });
-
-    document.addEventListener('click', (event) => {
-        const isClickInsideSearch = search.contains(event.target);
-        if (!isClickInsideSearch && search.classList.contains('active')) {
-            search.classList.remove('active');
-        }
-    });
 
     menuIcon.addEventListener('click', function () {
         mobileMenu.classList.toggle('show-menu');
