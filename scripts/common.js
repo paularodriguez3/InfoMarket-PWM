831a4074422e5770e6697d9246162ea827823a53
Raúl Núñez <raul.nunez101@alu.ulpgc.es>
2025-03-21 17:58:48 +0000

    Menú and nav-bar refactor

--- scripts/common.js
+++ scripts/common.js
@@ -16,14 +16,14 @@ async function loadTemplate(template, targetElementId, scriptPath = null) {
 }
 
 async function loadCommonTemplates() {
-    await loadTemplate('header-2/header-2.html', 'header-2', '../../templates/header-2/header.js');
     await loadTemplate('header/header.html', 'header', '../../templates/header/header.js');
+    await loadTemplate('nav-bar/nav-bar.html', 'nav-bar', '../../templates/nav-bar/nav-bar.js');
     await loadTemplate('footer/footer.html', 'footer');
-    await loadTemplate('shopping-info-component/shopping-info-component.html', 'shopping');
-    await loadTemplate('login-component/componente-inicio-sesion.html','sign-in');
-    await loadTemplate('login-component/componente-crear-cuenta.html','sign-up');
-    await loadTemplate('order-review-component/order-review-component.html', 'review');
-    await loadTemplate('User-component/Personal-profile-component.html', 'profile');
+    //await loadTemplate('shopping-info-component/shopping-info-component.html', 'shopping');
+    //await loadTemplate('login-component/componente-inicio-sesion.html','sign-in');
+    //await loadTemplate('login-component/componente-crear-cuenta.html','sign-up');
+    //await loadTemplate('order-review-component/order-review-component.html', 'review');
+    //await loadTemplate('User-component/Personal-profile-component.html', 'profile');
 }
 
 loadCommonTemplates();
--- templates/header/header.js
+++ templates/header/header.js
@@ -7,15 +7,27 @@ function waitForElement(selector, callback) {
     }
 }
 
-waitForElement("#menu-wrapper", () => {
-    const menuIcon = document.querySelector('.fi-rr-menu-burger');
-    const mobileMenu = document.querySelector('#menu-wrapper');
-    const main = document.querySelector('main');
-    const footer = document.querySelector('footer');
+waitForElement("#header-navigation", () => {
+    const search = document.querySelector('.search');
+    const btn = document.querySelector('.btn');
+    const input = document.querySelector('.input');
 
-    menuIcon.addEventListener('click', function () {
-        mobileMenu.classList.toggle('show-menu');
-        main.classList.toggle('blur-item');
-        footer.classList.toggle('blur-item');
+    btn.addEventListener('click', () => {
+        if (search.classList.contains('active')) {
+            search.classList.remove('active');
+            input.blur();
+        } else {
+            search.classList.add('active');
+            setTimeout(() => {
+                input.focus();
+            }, 50);
+        }
+    });
+
+    document.addEventListener('click', (event) => {
+        const isClickInsideSearch = search.contains(event.target);
+        if (!isClickInsideSearch && search.classList.contains('active')) {
+            search.classList.remove('active');
+        }
     });
 });
