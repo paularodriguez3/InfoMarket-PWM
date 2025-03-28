c340414d9b8eda52b7aa1d49d3a48293ae25e56c
paularodriguez3 <paularosarodriguezmorales03@gmail.com>
2025-03-28 10:56:36 +0000

    Sign up screen changed and responsive. Changes in common js

--- scripts/common.js
+++ scripts/common.js
@@ -8,7 +8,6 @@ async function loadTemplate(template, targetElementId, scriptPath = null) {
             const scriptElement = document.createElement("script");
             scriptElement.src = scriptPath;
             scriptElement.defer = true;
-            scriptElement.type = "module";
             document.body.appendChild(scriptElement);
         }
     } catch (error) {
@@ -29,15 +28,11 @@ async function loadCommonTemplates() {
     }
 
     if (pagina.endsWith('Sing-in.html')) {
-        await loadTemplate('login-component/componente-inicio-sesion.html','sign-in', '../templates/login-component/sign-in.js');
-    }
-
-    if (pagina.endsWith('Sing-up.html')) {
-        await loadTemplate('login-component/componente-crear-cuenta.html','sign-up', '../templates/login-component/sign-up.js');
+        await loadTemplate('login-component/componente-inicio-sesion.html','sign-in');
     }
 
     if (pagina.endsWith('Personal-profile.html')) {
-        await loadTemplate('User-component/Personal-profile-component.html', 'profile', '../templates/User-component/personal-profile.js');
+        await loadTemplate('User-component/Personal-profile-component.html', 'profile');
     }
 
     if (pagina.endsWith('order-review.html')) {
