ceb6f35b051a83091cdf9dba305b6a34060f03ff
Alberto <jesus.ortega.hernandez2002@gmail.com>
2025-04-21 16:24:03 +0100

    Sign-up and personal profile html and css

---
+++ src/app/pages/personal-profile/personal-profile.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-personal-profile',
+  standalone: false,
+  templateUrl: './personal-profile.component.html',
+  styleUrl: './personal-profile.component.css'
+})
+export class PersonalProfileComponent {
+
+}
