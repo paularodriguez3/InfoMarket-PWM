afab27eaf564d4127067706d268e27c483b3a2a2
paularodriguez3 <paularosarodriguezmorales03@gmail.com>
2025-04-21 16:22:34 +0100

    Sign in screen done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ import { ProductListComponent } from './pages/product-list/product-list.componen
 import {ShoppingCartComponent} from './pages/shopping-cart/shopping-cart.component';
 import {ClientSupportComponent} from './pages/client-support/client-support.component';
 import { ForgottenPasswordComponent } from './pages/forgotten-password/forgotten-password.component';
+import {SignInComponent} from './pages/sign-in/sign-in.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -14,6 +15,7 @@ const routes: Routes = [
   { path: 'shopping-cart', component: ShoppingCartComponent },
   { path: 'client-support', component: ClientSupportComponent },
   { path: 'forgotten-password', component: ForgottenPasswordComponent },
+  { path: 'sign-in', component: SignInComponent },
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import { ProductListComponent } from './pages/product-list/product-list.componen
 import { ShoppingCartComponent } from './pages/shopping-cart/shopping-cart.component';
 import { ClientSupportComponent } from './pages/client-support/client-support.component';
 import { ForgottenPasswordComponent } from './pages/forgotten-password/forgotten-password.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { ForgottenPasswordComponent } from './pages/forgotten-password/forgotten
     ProductListComponent,
     ShoppingCartComponent,
     ClientSupportComponent,
-    ForgottenPasswordComponent
+    ForgottenPasswordComponent,
+    SignInComponent
   ],
   imports: [
     BrowserModule,
