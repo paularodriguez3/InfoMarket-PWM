c21f5215c4d6f034b48d0d08c85fe89930c7d337
paularodriguez3 <paularosarodriguezmorales03@gmail.com>
2025-04-21 15:44:45 +0100

    Client support screen done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { ProductListComponent } from './pages/product-list/product-list.component';
 import {ShoppingCartComponent} from './pages/shopping-cart/shopping-cart.component';
+import {ClientSupportComponent} from './pages/client-support/client-support.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about-us', component: AboutUsComponent },
   { path: 'product-list/:category', component: ProductListComponent },
   { path: 'shopping-cart', component: ShoppingCartComponent },
+  { path: 'client-support', component: ClientSupportComponent },
 ];
 
 @NgModule({
