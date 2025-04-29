087b8a395a3f504f933d4206c1083944f13e32f5
juann45 <kikeelcaca@gmail.com>
2025-04-29 19:43:04 +0100

    Fixed order review components to recognize payment method

--- src/app/components/order-review-template/order-review-template.component.ts
+++ src/app/components/order-review-template/order-review-template.component.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+// src/app/components/order-review-template/order-review-template.component.ts
+
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-order-review-template',
-  standalone: true,
   templateUrl: './order-review-template.component.html',
-  styleUrl: './order-review-template.component.css'
+  styleUrls: ['./order-review-template.component.css']
 })
 export class OrderReviewTemplateComponent {
-
+  @Input() paymentMethod: string = '';
+  @Input() arrivalDate: string = '';
 }
