27a437460f5ad34d6b89d11fb0fac43bf4cd1f4b
Raúl Núñez <raul.nunez101@alu.ulpgc.es>
2025-04-30 13:32:49 +0100

    Shopping cart and shopping info update

--- src/app/components/shopping-info/shopping-info.component.ts
+++ src/app/components/shopping-info/shopping-info.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
-
+import { OnInit } from '@angular/core';
+import { ShoppingCartService } from '../../services/shopping-cart.service';
+import {ShoppingCartItem} from '../../models/shopping-cart-item.model';
+import {NgForOf} from '@angular/common';
+import {ProductInfoComponent} from '../product-info/product-info.component';
 @Component({
   selector: 'app-shopping-info',
   standalone: true,
   templateUrl: './shopping-info.component.html',
+  imports: [
+    NgForOf,
+    ProductInfoComponent
+  ],
   styleUrl: './shopping-info.component.css'
 })
-export class ShoppingInfoComponent {
+export class ShoppingInfoComponent implements OnInit {
+  shoppingCart: ShoppingCartItem[] = [];
+
+  constructor(private shoppingCartService: ShoppingCartService) {}
+
+  ngOnInit(): void {
+    this.shoppingCart = this.shoppingCartService.getCart();
+  }
+
+  calculateTotalPrice(): number {
+    let total:number = 0;
+    for (let item of this.shoppingCart) {
+      total += item.product.Precio * item.quantity;
+    }
+    return total;
+  }
 
 }
